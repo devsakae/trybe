bd0dfa490f692bccd981fca1cb95ea7bad1c181a
Rodrigo Sakae <devsakae@gmail.com>
2022-06-24 18:50:55 -0300

    Envio do arquivo de exercício 2

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercícios parte 1.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/Exercícios parte 1.js
@@ -48,4 +48,4 @@ leitor.livrosFavoritos.push({
 console.log(leitor);
 
 // 🚀 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
-console.log("Julia tem", leitor['livrosFavoritos'].length ,"livros favoritos");
+console.log(leitor.name, "tem", leitor['livrosFavoritos'].length ,"livros favoritos");
