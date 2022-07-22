4d56bb997e9c3c64298b1c2f196353ca4ab96784
Rodrigo Sakae <devsakae@gmail.com>
2022-07-22 18:09:43 -0300

    Fim da aula

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/aula.js
@@ -0,0 +1,4 @@
+//forEach => arr.forEach(currentValue, index, array)
+const myFunction = (p1, p2, p3) => console.log(`fruta: ${p1}, índice: ${p2}, total: ${p3}` );
+const fruits = ["apple", "orange", "cherry"];
+fruits.forEach(myFunction);
