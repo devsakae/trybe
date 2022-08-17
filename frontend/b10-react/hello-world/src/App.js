4e7ffc5307d0e2c0b6204bb7a8ea09c35e36b2f7
Rodrigo Sakae <devsakae@gmail.com>
2022-08-17 16:36:03 -0300

    Tirando b10

--- frontend/b10-react/hello-world/src/App.js
+++
@@ -1,9 +0,0 @@
-import './App.css';
-import Hello from './Hello'
-import Header from './Header';
-
-function App() {
-  return (<Header />)
-}
-
-export default App;
--- frontend/b10-react/hello-world/src/Hello.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class OlaMundo extends Component {
-  render() {
-    return <p class="OlaMundo">Olá, mundo!</p>
-  }
-}
-
-export default OlaMundo
