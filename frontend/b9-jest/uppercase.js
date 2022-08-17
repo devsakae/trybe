d1ef18d6351785f8ad5a9bdd2d7139866d50675c
Rodrigo Sakae <devsakae@gmail.com>
2022-08-17 16:30:21 -0300

    Apagando b9

--- frontend/b9-jest/uppercase.js
+++
@@ -1,8 +0,0 @@
-const uppercase = (str, callback) => {
-  // console.log(str);
-  setTimeout(() => {
-    callback(str.toUpperCase());
-  }, 500);
-};
-
-module.exports = uppercase;
--- frontend/b9-jest/uppercase.test.js
+++
@@ -1,8 +0,0 @@
-const uppercase = require('./uppercase')
-
-test('Testa a função Uppercase', (done) => {
-  uppercase('rodrigo', (param) => {
-    expect(param).toBe('RODRIGO');
-    done();
-  });
-});
